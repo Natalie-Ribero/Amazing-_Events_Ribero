048133bf12a608dbbe452c44e29d1dc478a1bb96
Natalie <natalie.ribero1@gmail.com>
2023-03-06 18:32:59 -0300

    agregue funcion para dinamizar las cards

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,4 +6,22 @@
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div> */
- 
+
+const mainHome = document.getElementById('cards')
+let tarjetas = ''
+function name(params) {
+  
+}
+for (let event of events) {
+  tarjetas += `<div class="card" style="width: 18rem;">
+    <img src=${event.image} alt="Imagen de evento">
+    <div class="card-body">
+     <h5 class="card-title">${event.name}</h5>     <p class="card-text">${event.description}</p>
+       <a href="mailto:${event.price}" class="btn btn-primary">Mandame un email</a>
+           </div>
+  </div>`
+}
+
+console.log(tarjetas);
+
+mainHome.innerHTML = tarjetas
