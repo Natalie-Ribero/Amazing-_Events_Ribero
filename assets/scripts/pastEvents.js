2d3e6788d5c6961818e1ae92cbb988c34fd2f99b
Natalie <natalie.ribero1@gmail.com>
2023-03-06 21:44:08 -0300

    Agregue contenido dinamico a past y upcoming

--- assets/scripts/pastEvents.js
+++ assets/scripts/pastEvents.js
@@ -1,1 +1,17 @@
 let pastEvents = events.filter(event => event.date < currentDate);
+
+let tarjetas = ''
+const divCards = document.getElementById('contenedorCards');
+
+for (let event of pastEvents) {
+  tarjetas += `<div class="card estilocard" style="width: 18rem;">
+    <img src= ${event.image} alt="Imagen de evento">
+    <div class="card-body">
+    <h5 class="card-title">${event.name}</h5>     
+    <p class="card-text">${event.description}</p>
+    <a href="mailto:${event.price}" class="btn btn-primary">Mandame un email</a>
+    </div>
+  </div>`
+}
+divCards.innerHTML = tarjetas
+console.log(tarjetas);
--- assets/scripts/upcomingEvents.js
+++ assets/scripts/upcomingEvents.js
@@ -1,1 +1,17 @@
 let upcomingEvents = events.filter(event => event.date > currentDate);
+
+let tarjetas = ''
+const divCards = document.getElementById('contenedorCards');
+
+for (let event of upcomingEvents) {
+  tarjetas += `<div class="card estilocard" style="width: 18rem;">
+    <img src= ${event.image} alt="Imagen de evento">
+    <div class="card-body">
+    <h5 class="card-title">${event.name}</h5>     
+    <p class="card-text">${event.description}</p>
+    <a href="mailto:${event.price}" class="btn btn-primary">Mandame un email</a>
+    </div>
+  </div>`
+}
+divCards.innerHTML = tarjetas
+console.log(tarjetas);
