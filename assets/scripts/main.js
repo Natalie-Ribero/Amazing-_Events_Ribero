417698a4fb58aa32659dcfbd3414c13aea95571a
Natalie-Ribero <natalie.ribero1@gmail.com>
2023-03-11 17:07:14 -0300

    desarrollo de logica para caategory

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,62 +1,69 @@
-let cards = ''
-const divCards = document.getElementById('contenedorCards');
+// function armarCards(array) {
+//   for (let event of array) {
+//     cards += `<div class="card estilocard" style="width: 18rem;">
+//       <img src= ${event.image} alt="Imagen de evento">
+//       <div class="card-body">
+//       <h5 class="card-title">${event.name}</h5>
+//       <h6 class="card-title">${event.date}</h6>    
+//       <h6 class="card-title">${event.price} USD</h6>     
+//       <p class="card-text">${event.description}</p>
+//       <a class="btn btn-primary">See more</a>
+//       </div>
+//     </div>`
+//   }
+// }
+// armarCards(events);
 
-function armarCards(array) {
-  for (let event of array) {
-    cards += `<div class="card estilocard" style="width: 18rem;">
-      <img src= ${event.image} alt="Imagen de evento">
-      <div class="card-body">
-      <h5 class="card-title">${event.name}</h5>
-      <h6 class="card-title">${event.date}</h6>    
-      <h6 class="card-title">${event.price} USD</h6>     
-      <p class="card-text">${event.description}</p>
-      <a class="btn btn-primary">See more</a>
-      </div>
-    </div>`
+// divCards.innerHTML = cards;
+
+const food = document.getElementById('food');
+const museum = document.getElementById('museum');
+const party = document.getElementById('party');
+const concert = document.getElementById('concert');
+const race = document.getElementById('race');
+const book = document.getElementById('book');
+const cinema = document.getElementById('cinema');
+const buscar = document.getElementById('buscar');
+const boton = document.getElementById('boton');
+
+const tarjetas = [];
+
+function armarCards(event) {
+  tarjetas += `<div class="card estilocard" style="width: 18rem;">
+  <img src= ${event.image} alt="Imagen de evento">
+  <div class="card-body">
+  <h5 class="card-title">${event.name}</h5>
+  <h6 class="card-title">${event.date}</h6>    
+  <h6 class="card-title">${event.price} USD</h6>     
+  <p class="card-text">${event.description}</p>
+  <a class="btn btn-primary">See more</a>
+  </div>
+</div>`
+}
+
+for (let event of events) {
+  if (food.checked && event.category === "Food Fair") {
+    armarCards(event)
   }
+  if (museum.checked && event.category === "Museum") {
+    armarCards(event)
+  }
+  if (party.checked && event.category === "Costume Party") {
+    armarCards(event)
+  }
+  if (concert.checked && event.category === "Music Concert") {
+    armarCards(event)
+  }
+  if (race.checked && event.category === "Race") {
+    armarCards(event)
+  }
+  if (book.checked && event.category === "Book Exchange") {
+    armarCards(event)
+  }
+  if (cinema.checked && event.category === "Cinema") {
+    armarCards(event)
+  }
+  document.querySelector('#categorias')
 }
-armarCards(events);
-
-divCards.innerHTML = cards;
-
-let labels = Array.from(document.querySelectorAll('input[type="checkbox"]'))
-
-function filtrarCardsCategory(condicion) {
-  let filtroCategory = events.filter(event => event.category === condicion)
-  return filtroCategory
-}
-
-labels[0, 1, 2, 3, 4, 5, 6].addEventListener('click', ()=> {
-  filtrarCardsCategory(labels.value)
-})
-
-
-// labels[0].addEventListener('change', (e) => {
-//     armarCards(foodFair);
-// })
-
-// labels[1].addEventListener('change', (e) => {
-//   armarCards(museum)
-// })
-
-// labels[2].addEventListener('change', (e) => {
-//   armarCards(customeParty)
-// })
-
-// labels[6].addEventListener('change', (e) => {
-//   armarCards(musicConcert)
-// })
-
-// labels[4].addEventListener('change', (e) => {
-//   armarCards(race)
-// })
-
-// labels[5].addEventListener('change', (e) => {
-//   armarCards(bookExchange)
-// })
-
-// labels[6].addEventListener('change', (e) => {
-//   armarCards(cinema)
-// })
 
 
