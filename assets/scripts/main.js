8df26c5fccfba1734c0c5b58ce6fe2bca03f9520
Natalie-Ribero <natalie.ribero1@gmail.com>
2023-03-12 20:21:25 -0300

    Logica de category intento 3

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,76 +1,52 @@
-// function armarCards(array) {
-//   for (let event of array) {
-//     cards += `<div class="card estilocard" style="width: 18rem;">
-//       <img src= ${event.image} alt="Imagen de evento">
-//       <div class="card-body">
-//       <h5 class="card-title">${event.name}</h5>
-//       <h6 class="card-title">${event.date}</h6>    
-//       <h6 class="card-title">${event.price} USD</h6>     
-//       <p class="card-text">${event.description}</p>
-//       <a class="btn btn-primary">See more</a>
-//       </div>
-//     </div>`
-//   }
-// }
-// armarCards(events);
+let cards = ''
+const divCards = document.getElementById('contenedorCards');
 
-// divCards.innerHTML = cards;
+for (let event of events) {
+  cards += `<div class="card estilocard" style="width: 18rem;">
+    <img src= ${event.image} alt="Imagen de evento">
+    <div class="card-body">
+    <h5 class="card-title">${event.name}</h5> 
+    <h6 class="card-title">${event.price} USD</h6>       
+    <p class="card-text">${event.description}</p>
+    <a class="btn btn-primary">See more</a>
+    </div>
+  </div>`
+}
+divCards.innerHTML = cards;
 
-let food = document.querySelector('#food');
-let museum = document.querySelector('#museum');
-let party = document.querySelector('#party');
-let concert = document.querySelector('#concert');
-let race = document.querySelector('#race');
-let book = document.querySelector('#book');
-let cinema = document.querySelector('#cinema');
-
-let buscar = document.querySelector('#buscar');
+// hacer aparecer cards con category
 let boton = document.querySelector('#boton');
 
-let tarjetas = [];
+boton.addEventListener('click', (e) =>{
+e.preventDefault()
+let labels = document.querySelector('#labels')
 
-function armarCards(event) {
-   `<div class="card estilocard" style="width: 18rem;">
-  <img src= ${event.image} alt="Imagen de evento">
-  <div class="card-body">
-  <h5 class="card-title">${event.name}</h5>
-  <h6 class="card-title">${event.date}</h6>    
-  <h6 class="card-title">${event.price} USD</h6>     
-  <p class="card-text">${event.description}</p>
-  <a class="btn btn-primary">See more</a>
-  </div>
-</div>`
+
+let buscar = document.querySelector('#buscar');
+let cardsContainer = document.querySelector('#contenedorCards')
+
+let category = events.filter(event => (event.category === "Food Fair" && food.checked || event.category === "Museum" && museum.checked || event.category === "Costume Party" && party.checked || event.category === "Music Concert" && concert.checked || event.category === "Race" && race.checked || event.category === "Book Exchange" && book.checked || event.category === "Cinema" && cinema.checked))
+
+let cards = '' 
+for (let event of category) {
+  cards += `<div class="card estilocard" style="width: 18rem;">
+    <img src= ${event.image} alt="Imagen de evento">
+    <div class="card-body">
+    <h5 class="card-title">${event.name}</h5> 
+    <h6 class="card-title">${event.price} USD</h6>       
+    <p class="card-text">${event.description}</p>
+    <a class="btn btn-primary">See more</a>
+    </div>
+  </div>`
 }
+divCards.innerHTML = cards;
+
+})
+// hasta aca funciona
 
 
-// for (let event of events) {
-//   if (food.checked && event.category === "Food Fair") {
-//     armarCards(event)
-//   }
 
-//   if (museum.checked && event.category === "Museum") {
-//     armarCards(event)
-//   }
 
-//   if (party.checked && event.category === "Costume Party") {
-//     armarCards(event)
-//   }
 
-//   if (concert.checked && event.category === "Music Concert") {
-//     armarCards(event)
-//   }
 
-//   if (race.checked && event.category === "Race") {
-//     armarCards(event)
-//   }
-
-//   if (book.checked && event.category === "Book Exchange") {
-//     armarCards(event)
-//   }
-
-//   if (cinema.checked && event.category === "Cinema") {
-//     armarCards(event)
-//   }
-// }
-document.querySelector('#contenedorCards').innerHTML = tarjetas
 
