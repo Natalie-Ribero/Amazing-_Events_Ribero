573dc9f67700090a2c933852a291427729c1743d
Natalie <natalie.ribero1@gmail.com>
2023-03-17 12:54:54 -0300

    pintar checkbox

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,20 @@
 let url = "https://mindhub-xj03.onrender.com/api/amazing"
 const divCards = document.getElementById('contenedorCards');
-let food = document.querySelector('#FoodFair');
-let museum = document.querySelector('#Museum');
-let party = document.querySelector('#CostumeParty');
-let concert = document.querySelector('#MusicConcert');
-let race = document.querySelector('#Race');
-let book = document.querySelector('#BookExchange');
-let cinema = document.querySelector('#Cinema');
-
+let labels = document.getElementById('labels1');
+let cards = ''
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
     sumarCardsArray(data.events)
+    crearCheckbox(data.events)
+    filtroCategorys(data.events)
   })
- 
-  function sumarCardsArray(array) {
-    let cards = ''
-      for (let event of array) {
-        cards += `<div class="card estilocard" style="width: 18rem;">
+
+function sumarCardsArray(array) {
+
+  for (let event of array) {
+    cards += `<div class="card estilocard" style="width: 18rem;">
         <img src= ${event.image} alt="Imagen de evento">
         <div class="card-body">
         <h5 class="card-title">${event.name}</h5>
@@ -28,11 +24,22 @@ fetch(url)
         <a href="./details.html?id=${event._id}" class="btn btn-primary">See more</a>
         </div>
       </div>`
-      }
-      divCards.innerHTML = cards;
-    }
-    
+  }
+  divCards.innerHTML = cards;
+}
 
+function crearCheckbox(array) {
+  let categorias = array.map(event => event.category);
+  let category = new Set(categorias);
+  let checkbox = ''
+  for (let event of category) {
+    checkbox +=
+      `<label> ${event}
+  <input id= '${event.replace(/\s+/g, '')}' type="checkbox">
+</label>`
+  }
+  labels.innerHTML = checkbox;
+}
 
 
 
