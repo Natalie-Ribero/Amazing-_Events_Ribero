8ffed9f469ef7a282dff7fa12afcfdaa3f7791a3
Natalie-Ribero <natalie.ribero1@gmail.com>
2023-03-17 17:00:15 -0300

    no me sale

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,49 +1,146 @@
-let url = "https://mindhub-xj03.onrender.com/api/amazing"
-const divCards = document.getElementById('contenedorCards');
-let labels = document.getElementById('labels1');
-let cards = ''
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    sumarCardsArray(data.events)
-    crearCheckbox(data.events)
-    filtroCategorys(data.events)
-  })
-
-function sumarCardsArray(array) {
-
-  for (let event of array) {
-    cards += `<div class="card estilocard" style="width: 18rem;">
-        <img src= ${event.image} alt="Imagen de evento">
-        <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <h6 class="card-title"> ${event.date}</h6>
-        <h6 class="card-title">${event.price} USD</h6>
-        <p class="card-text">${event.description}</p>
-        <a href="./details.html?id=${event._id}" class="btn btn-primary">See more</a>
-        </div>
-      </div>`
-  }
-  divCards.innerHTML = cards;
-}
-
-function crearCheckbox(array) {
-  let categorias = array.map(event => event.category);
-  let category = new Set(categorias);
-  let checkbox = ''
-  for (let event of category) {
-    checkbox +=
-      `<label> ${event}
-  <input id= '${event.replace(/\s+/g, '')}' type="checkbox">
-</label>`
-  }
-  labels.innerHTML = checkbox;
-}
 
 
+  //   // Crear categorys
+  //   let labels = document.getElementById('labels1');
+  //   let categorias = data.events.map(event => event.category);
+  //   let category = new Set(categorias);
+  //   let checkbox = ''
+  //   for (let event of category) {
+  //     checkbox +=
+  //       `<label> ${event}
+  //     <input id= '${event.replace(/\s+/g, '')}' type="checkbox">
+  //   </label>`
+  //   }
+  //   labels.innerHTML = checkbox;
+    
+    
+  //   //Traer los category por su id
+  //   let food = document.querySelector('#FoodFair');
+  //   let museum = document.querySelector('#Museum');
+  //   let party = document.querySelector('#CostumeParty');
+  //   let concert = document.querySelector('#MusicConcert');
+  //   let race = document.querySelector('#Race');
+  //   let book = document.querySelector('#BookExchange');
+  //   let cinema = document.querySelector('#Cinema');
+  //   const divCards = document.getElementById('contenedorCards');
+    
+    
+  //   // //Pintar tarjetas con los datos de data
+  //   function sumarCardsArray(array) {
+  //     let cards = ''
+  //     for (let event of array) {
+  //       cards += `<div class="card estilocard" style="width: 18rem;">
+  //       <img src= ${event.image} alt="Imagen de evento">
+  //       <div class="card-body">
+  //       <h5 class="card-title">${event.name}</h5>
+  //       <h6 class="card-title"> ${event.date}</h6>
+  //       <h6 class="card-title">${event.price} USD</h6>
+  //       <p class="card-text">${event.description}</p>
+  //       <a href="./details.html?id=${event._id}" class="btn btn-primary">See more</a>
+  //       </div>
+  //     </div>`
+  //     }
+  //   }
+    
+  //   sumarCardsArray(data.events);
+    
+  //   divCards.innerHTML = cards;
+    
+  //   //Filtro de Categorys
+  //   document.addEventListener('click', (e) => {
+    
+  //     if (food.checked || museum.checked || party.checked || concert.checked || race.checked || book.checked || cinema.checked) {
+    
+  //       let category = data.events.filter(event => (event.category === "Food Fair" && food.checked || event.category === "Museum" && museum.checked || event.category === "Costume Party" && party.checked || event.category === "Music Concert" && concert.checked || event.category === "Race" && race.checked || event.category === "Book Exchange" && book.checked || event.category === "Cinema" && cinema.checked))
+    
+  //       cards = ''
+  //       sumarCardsArray(category);
+  //       divCards.innerHTML = cards;
+    
+  //     } else {
+  //       cards = ''
+  //       sumarCardsArray(data.events);
+  //       divCards.innerHTML = cards;
+  //     }
+  //   })
+    
+    
+  //   document.addEventListener("keyup", (e) => {
+    
+  //     if (e.target.matches("#buscador")) {
+    
+  //       document.querySelectorAll(".estilocard").forEach(tarjeta => {
+    
+  //         tarjeta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+  //           ? tarjeta.classList.remove("filtro")
+  //           : tarjeta.classList.add("filtro")
+  //       })
+  //     }
+  //   })
+  // })
 
 
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// function crearCheckbox(array) {
+//   let categorias = array.map(event => event.category);
+//   let category = new Set(categorias);
+//   let checkbox = ''
+//   for (let event of category) {
+//     checkbox +=
+//       `<label> ${event}
+//        <input id= '${event.replace(/\s+/g, '')}' type="checkbox" name = ${event.toLowerCase()}>
+//        </label>`
+//   }
+//   labels.innerHTML = checkbox;
+// }
+
+// function sumarCardsArray(array) {
+
+//   for (let event of array) {
+//     cards += `<div class="card estilocard" style="width: 18rem;">
+//         <img src= ${event.image} alt="Imagen de evento">
+//         <div class="card-body">
+//         <h5 class="card-title">${event.name}</h5>
+//         <h6 class="card-title"> ${event.date}</h6>
+//         <h6 class="card-title">${event.price} USD</h6>
+//         <p class="card-text">${event.description}</p>
+//         <a href="./details.html?id=${event._id}" class="btn btn-primary">See more</a>
+//         </div>
+//       </div>`
+//   }
+//   divCards.innerHTML = cards;
+// }
+
+
+// function filtroCategorys(array) {
+
+//   let food = document.querySelector('#FoodFair');
+//   let museum = document.querySelector('#Museum');
+//   let party = document.querySelector('#CostumeParty');
+//   let concert = document.querySelector('#MusicConcert');
+//   let race = document.querySelector('#Race');
+//   let book = document.querySelector('#BookExchange');
+//   let cinema = document.querySelector('#Cinema');
+//   document.addEventListener('click', (e) => {
+
+//     if (food.checked || museum.checked || party.checked || concert.checked || race.checked || book.checked || cinema.checked) {
+
+//       let category = array.filter(event => (event.category === "Food" && food.checked || event.category === "Museum" && museum.checked || event.category === "Costume Party" && party.checked || event.category === "Concert" && concert.checked || event.category === "Race" && race.checked || event.category === "Book" && book.checked || event.category === "Cinema" && cinema.checked))
+
+//       cards = ''
+//       sumarCardsArray(category);
+//       divCards.innerHTML = cards;
+
+//     } else {
+//       cards = ''
+//       sumarCardsArray(array);
+//       divCards.innerHTML = cards;
+//     }
+//   })
+// }
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // // Crear categorys
 // let labels = document.getElementById('labels1');
